a79dcc7c65e69a8d78d56c133a6449861937b57e
hero <hero@asterisk-works.jp>
2016-10-08 13:25:24 +0900

    modify display design

--- weather_jp.tingapp/main.py
+++ weather_jp.tingapp/main.py
@@ -1,37 +1,15 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 import tingbot
 from tingbot import *
 import requests
 from jp_utils import jptime
+from weather import *
 
 # setup code here
-font = tingbot.app.settings['font']
+
 params = { 'city': tingbot.app.settings['city'] }
 url = "http://weather.livedoor.com/forecast/webservice/json/v1"
-
 screen.brightness = 70
-
-def templature(forcast):
-    t_min = '-'
-    t_max = '-'
-    if forcast['temperature']['min'] != None:
-        t_min = forcast['temperature']['min']['celsius']
-    if forcast['temperature']['max'] != None:
-        t_max = forcast['temperature']['max']['celsius']
-    return { 'min': t_min, 'max': t_max }
-    
-
-# display weather
-# @param forcast [dict] forcast
-def disp_weather(forcast):
-    screen.text(u'%(dateLabel)s %(date)s' % forcast, xy=(5,50), align='topleft', font=font)
-    screen.text(u'%(telop)s' % forcast, xy=(110,100), align='topleft', font=font, font_size=50)
-    screen.image(forcast['image']['url'], scale=2, xy=(10,100), align='topleft')
-    
-    temp = templature(forcast)
-    screen.text(u'%(min)s ℃' % temp, color='blue', font=font, xy=(145, 180), align='topright')
-    screen.text(' / ', color='black', font=font, xy=(160, 180), align='top')
-    screen.text(u'%(max)s ℃' % temp, color='red', font=font, xy=(175, 180), align='topleft')
 
 @every(minutes=6)
 def loop():
@@ -46,9 +24,9 @@ def loop():
     
     forecast = today if current_hour < 21 else tomorrow
     # draw screen ##########
-    screen.fill(color='white')
-    screen.text( '%(prefecture)s %(city)s' % location,
-                font=font, color='black', xy=(5,5), align='topleft')
+    screen.fill(color=(240,240,240))
+    screen.text( '%(prefecture)s\n%(city)s' % location,
+                font=font, font_size=20, color='black', xy=(310,10), align='topright')
     disp_weather(forecast)
     # copy right
     screen.text(u'%(provider)s / %(title)s' % copyright, xy=(160,240), align='bottom', font=font, font_size=14)
---
+++ weather_jp.tingapp/weather.py
@@ -0,0 +1,92 @@
+# coding: utf-8
+import tingbot
+from tingbot import *
+
+font = tingbot.app.settings['font']
+wfont ='fonts/iconvault_forecastfont.ttf'
+
+def templature(forcast):
+    t_min = '-'
+    t_max = '-'
+    if forcast['temperature']['min'] != None:
+        t_min = forcast['temperature']['min']['celsius']
+    if forcast['temperature']['max'] != None:
+        t_max = forcast['temperature']['max']['celsius']
+    return { 'min': t_min, 'max': t_max }
+    
+def weather_icon(url):
+    sunny  = {'font': u'\uf101', 'color':(255, 165, 0)}
+    sun  = {'font': u'\uf113', 'color':(255, 165, 0)}
+    base_cloud = {'font': u'\uf105', 'color':(180,180,180)}
+    cloudy = {'font': u'\uf106', 'color':(180,180,180)}
+    rain   = {'font': u'\uf107', 'color':(100,149,237)}
+    windyrain = {'font': u'\uf10e', 'color':(100,149,237)}
+    sleet   = {'font': u'\uf10c', 'color':(100,149,237)}
+    snow   = {'font': u'\uf102', 'color':(133, 216, 247)}
+    snowy   = {'font': u'\uf10b', 'color':(133, 216, 247)}
+    windysnow = {'font': u'\uf103', 'color':(133, 216, 247)}
+    windy  = {'font': u'\uf115', 'color':(139,137,137)}
+    windyraincloud = {'font': u'\uf111', 'color':(139,137,137)}
+    windysnowcloud = {'font': u'\uf109', 'color':(139,137,137)}
+    rain_storm = {'font': u'\uf018', 'color':(0,0,255)}
+    snow_storm = {'font': u'\uf064', 'color':(0,0,255)}
+    file = url.split('/')[-1].split('.')[-2]
+    code = { '1': [[sun]], # 晴
+             '2': [[sunny,cloudy]], # 晴れ時々曇り
+             '3': [[base_cloud, sunny, rain]], # 晴れ時々雨
+             '4': [[base_cloud, sunny, snow]], # 晴れ時々雪
+             '5': [[sun],[cloudy]], # 晴れのち曇り
+             '6': [[sun],[base_cloud, rain]], # 晴れのち雨
+             '7': [[sun],[base_cloud, snowy]], # 晴れのち雪
+             '8': [[cloudy]], # 曇り
+             '9': [[sunny,cloudy]], # 曇り時々晴れ
+             '10': [[base_cloud, rain]], # 曇り時々雨
+             '11': [[base_cloud, snow]], #曇り時々雪
+             '12': [[cloudy], [sun]], # 曇りのち晴れ
+             '13': [[cloudy], [base_cloud, rain]], # 曇りのち雨
+             '14': [[cloudy], [base_cloud, snowy]], # 曇りのち雪
+             '15': [[rain]], # 雨
+             '16': [[base_cloud, rain, sunny]], # 雨時々晴れ
+             '17': [[base_cloud, rain]], # 雨時々止む(曇り)
+             '18': [[base_cloud, sleet]], # 雨時々雪
+             '19': [[base_cloud, rain],[sun]], # 雨のち晴れ
+             '20': [[base_cloud, rain],[cloudy]], # 雨のち曇り
+             '21': [[base_cloud, rain],[base_cloud, snowy]], # 雨のち雪
+             '22': [[windyraincloud, windyrain]], # 雨で暴風を伴う
+             '23': [[base_cloud, snowy]], # 雪
+             '24': [[base_cloud, snow, sunny]], # 雪時々晴れ
+             '25': [[base_cloud, windysnow]], # 雪時々止む(曇り)
+             '26': [[base_cloud, sleet]], # 雪時々雨
+             '27': [[base_cloud, snowy],[sun]], # 雪のち晴れ
+             '28': [[base_cloud, snowy],[cloudy]], # 雪のち曇り
+             '29': [[base_cloud, snowy],[rain]], # 雪のち雨
+             '30': [[windysnowcloud, windysnow]] # 暴風雪
+            }[file]
+    return code
+    
+def disp_font(icon, xy=(160,120), font_size=270, align='center'):
+    for i in icon:
+        screen.text(i['font'], xy=xy, font_size=font_size, align=align, font=wfont, color=i['color'])
+
+def disp_icon(forcast):
+    icon=weather_icon(forcast['image']['url'])
+    if len(icon) > 1:
+        disp_font(icon[0], xy=(200,160), font_size=150, align='bottomright')
+        disp_font(icon[1], xy=(120,80), font_size=150, align='topleft')
+        # TODO sometime or after
+    else:
+        disp_font(icon[0])
+      
+# display weather
+# @param forcast [dict] forcast
+def disp_weather(forcast):
+    disp_icon(forcast)
+    
+    screen.text(u'%(date)s\n%(dateLabel)s' % forcast, xy=(10,10), align='topleft', font_size=30, font=font, color='black')
+    screen.text(u'%(telop)s' % forcast, xy=(310,230), align='bottomright', font=font, font_size=60, color='black')
+
+    # templature
+    temp = templature(forcast)
+    screen.text(u'%(min)s ℃' % temp, color='blue', font=font, xy=(310, 110), align='topright')
+    # screen.text(' / ', color='black', font=font, xy=(160, 140), align='top')
+    screen.text(u'%(max)s ℃' % temp, color='red', font=font, xy=(310, 140), align='topright')
